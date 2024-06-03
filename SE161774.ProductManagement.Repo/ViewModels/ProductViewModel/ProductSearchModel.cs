143ebdb709f0669eae7a835a411ec603d850b532
NguyenChienThang <chienthang014@gmail.com>
2024-06-03 16:22:46 +0700

    update model

--- SE161774.ProductManagement.Repo/ViewModels/ProductViewModel/ProductSearchModel.cs
+++ SE161774.ProductManagement.Repo/ViewModels/ProductViewModel/ProductSearchModel.cs
@@ -11,20 +11,19 @@ namespace SE161774.ProductManagement.Repo.ViewModels.ProductViewModel
     {
         [JsonPropertyName("page-index")]
         public int? page_index { get; set; }
+
         [JsonPropertyName("page-size")]
-
         public int? page_size { get; set; }
+
         [JsonPropertyName("product-name")]
-
         public string? product_name { get; set; }
+
         [JsonPropertyName("unit-price-minx")]
-
         public decimal? unit_price_min { get; set; }
-        [JsonPropertyName("unit-price-max")]
 
+        [JsonPropertyName("unit-price-max")]
         public decimal? unit_price_max { get; set; }
         [JsonPropertyName("sort-by-price")]
-
         public bool? sort_by_price { get; set; } = false;
         public bool? descending { get; set; } = false;
         
